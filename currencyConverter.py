a1936edde34767b9de8dffbf30970ce8f3f5b860
Giordano Bruno <giordanobrunotj@hotmail.com>
2023-06-30 19:19:27 +0100

    add read me

--- currencyConverter.py
+++ currencyConverter.py
@@ -5,8 +5,7 @@ class currencyCorverter:
 
 cur = input("what currency do you want to convert? ")
 
-reader = open("currencies.txt", "r")
+reader = open("currencies.txt", "r") 
 curList = reader.read().split("\n")
 print(curList)
 for i in range(len(curList)):
-    
